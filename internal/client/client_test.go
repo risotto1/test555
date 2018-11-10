6d202297209d02bbf420dfe1f23d2ee037fcd926
GoingFast <laheristo@gmail.com>
2018-11-10 13:14:00 +0200

    dasda

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -17,7 +17,7 @@ func (m mockConns) Read(ctx context.Context, in *empty.Empty, opts ...grpc.CallO
 	return m.OnRead(ctx, in)
 }
 
-func estRead(t *testing.T) {
+func stRead(t *testing.T) {
 	// tests := []struct {
 	// 	wantBody   interface{}
 	// 	wantStatus int
