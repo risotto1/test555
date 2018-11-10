87ccda0f6bc5d7ae67debe95be35399cd08678e2
GoingFast <laheristo@gmail.com>
2018-11-10 12:19:07 +0200

    test11

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -17,7 +17,7 @@ func (m mockConns) Read(ctx context.Context, in *empty.Empty, opts ...grpc.CallO
 	return m.OnRead(ctx, in)
 }
 
-func TestRead(*testing.T) {
+func estRead(*testing.T) {
 	// tests := []struct {
 	// 	wantBody   interface{}
 	// 	wantStatus int
